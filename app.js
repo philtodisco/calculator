6d2038568776752a4d255d4eb8cd0e0dfafdfec3
Phil Todisco <philtodisco@phils-mbp.lan>
2022-01-03 21:11:07 -0500

    Add operate function

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
-// add function 
 let add = (a, b) => {
     return a + b
     console.log()
 } 
 
-// subract function
 let subtract = (a, b) => {
     return a - b 
 } 
 
-// multiply function
 let multiply = (a, b) => {
     return a * b
 } 
 
-// divide function
 let divide = (a, b) => {
     return a / b
 } 
 
-//operator tests
-console.log(add(2,2), "this should be 4")
-console.log(subtract(2,2), "this should be 0")
-console.log(multiply(2,2), "this should be 4")
-console.log(divide(2,2), "this should be 1")
+let operate = (operator, a, b) => {
+    if (operator === add) {return add(a,b)}
+    if (operator === subtract) {return subtract(a,b)}
+    if (operator === multiply) {return multiply(a,b)}
+    if (operator === divide) {return divide(a,b)}
+}
 
+// operator tests
+// console.log(add(2,2), "this should be 4")
+// console.log(subtract(2,2), "this should be 0")
+// console.log(multiply(2,2), "this should be 4")
+// console.log(divide(2,2), "this should be 1")
