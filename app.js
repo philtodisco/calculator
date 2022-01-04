e709205b274668d3d75b941707845719ab6be502
Phil Todisco <philtodisco@phils-mbp.lan>
2022-01-03 20:21:14 -0500

    Add operation functions

---
+++ app.js
@@ -0,0 +1,11 @@
+// add function 
+let add = (a, b) => a + b
+
+// subract function
+let subtract = (a, b) => a - b
+
+// multiply function
+let multiply = (a, b) => a * b
+
+// divide function
+let divide = (a, b) => a / b
