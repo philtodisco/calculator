4358b65d45d40ed56069e19835d7850ca6333efe
Phil Todisco <philtodisco@phils-mbp.lan>
2022-01-03 20:43:45 -0500

    Add tests

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 // add function 
 let add = (a, b) => {
-    return a + b  
+    return a + b
+    console.log()
 } 
 
 // subract function
 let subtract = (a, b) => {
     return a - b 
 } 
-
-
 
 // multiply function
 let multiply = (a, b) => {
@@ -19,4 +18,10 @@ let multiply = (a, b) => {
 let divide = (a, b) => {
     return a / b
 } 
+
+//operator tests
+console.log(add(2,2), "this should be 4")
+console.log(subtract(2,2), "this should be 0")
+console.log(multiply(2,2), "this should be 4")
+console.log(divide(2,2), "this should be 1")
 
