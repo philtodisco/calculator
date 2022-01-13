e04d3dafbccb99d081ae6aacd3e86387d40cc461
Phil Todisco <philtodisco@phils-mbp.lan>
2022-01-12 21:24:25 -0500

    Add chooseOperator function

--- app.js
+++ app.js
@@ -7,8 +7,16 @@ let equalsButton = document.querySelector('[data-equals]')
 let previousOperandTextEl = document.querySelector('[data-previous-operand]')
 let currentOperandTextEl = document.querySelector('[data-current-operand]')
 
+let currentOperator = ''
+let firstOperand = ''
+let secondOperand = ''
+
 appendNumber = (number) => {
     currentOperandTextEl.innerText = currentOperandTextEl.innerText += number
+    let firstOperand = currentOperandTextEl.innerText
+}
+chooseOperator = (operator) => {
+    currentOperator = operator
 }
 
 numberButtons.forEach((button) => {
@@ -19,33 +27,33 @@ numberButtons.forEach((button) => {
 
 operationButtons.forEach((button) => {
     button.addEventListener('click', () => {
-        
+        chooseOperator(button.innerText)
     })
 })
 
 
-//OPERATORS
-let add = (a, b) => {
-    return a + b
-    console.log()
-} 
+// //OPERATORS
+// let add = (a, b) => {
+//     return a + b
+//     console.log()
+// } 
 
-let subtract = (a, b) => {
-    return a - b 
-} 
+// let subtract = (a, b) => {
+//     return a - b 
+// } 
 
-let multiply = (a, b) => {
-    return a * b
-} 
+// let multiply = (a, b) => {
+//     return a * b
+// } 
 
-let divide = (a, b) => {
-    return a / b
-} 
+// let divide = (a, b) => {
+//     return a / b
+// } 
 
-let operate = (operator, a, b) => {
-    if (operator === add) {return add(a,b)}
-    if (operator === subtract) {return subtract(a,b)}
-    if (operator === multiply) {return multiply(a,b)}
-    if (operator === divide) {return divide(a,b)}
-}
+// let operate = (operator, a, b) => {
+//     if (operator === add) {return add(a,b)}
+//     if (operator === subtract) {return subtract(a,b)}
+//     if (operator === multiply) {return multiply(a,b)}
+//     if (operator === divide) {return divide(a,b)}
+// }
 
