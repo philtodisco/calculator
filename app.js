2a76c2dd7b4d163f2d9f95db69440dd5d182fc8f
Phil Todisco <philtodisco@phils-mbp.lan>
2022-01-24 20:01:51 -0500

    Fix repeat operation bug

--- app.js
+++ app.js
@@ -17,15 +17,16 @@ appendNumber = (number) => {
     if (currentOperandTextEl.innerText.length > 10) return
     if (currentOperandTextEl.innerText === '' && number === '0') return
     currentOperandTextEl.innerText = currentOperandTextEl.innerText += number
+    
 
     //comma logic ???
-    currentOperandTextEl.innerText = parseFloat(currentOperandTextEl.innerText.replace(/,/g, ''));
-    currentOperandTextEl.innerText = Number(currentOperandTextEl.innerText).toLocaleString()
+    // currentOperandTextEl.innerText = parseFloat(currentOperandTextEl.innerText.replace(/,/g, ''));
+    // currentOperandTextEl.innerText = Number(currentOperandTextEl.innerText).toLocaleString()
  
 }
 
 appendDecimal = () => {
-    if (currentOperandTextEl.innerText === '') { 
+    if (currentOperandTextEl.innerText === '') {  
         currentOperandTextEl.innerText = '0.'
     } else if (currentOperandTextEl.innerText.includes('.')) {
         return
@@ -50,19 +51,23 @@ allClear = () => {
     currentOperator = ''
     firstOperand = ''
     secondOperand = ''
-    previousOperandTextEl.innerText = ''
-    currentOperandTextEl.innerText = ''
-}
-
-deleteNumber = () => {
-    currentOperandTextEl.innerText = currentOperandTextEl.innerText.slice(0, -1)
-}
-
-equals = (results) => {
-    secondOperand = currentOperandTextEl.innerText
-    operate(currentOperator, firstOperand, secondOperand)
     resetDisplays()
-    currentOperandTextEl.innerText = firstOperand
+}
+
+deleteNumber = () => currentOperandTextEl.innerText = currentOperandTextEl.innerText.slice(0, -1)
+
+equals = () => {
+    // repeat operation
+    if (previousOperandTextEl.innerText == '') {
+        firstOperand = currentOperandTextEl.innerText
+        operate(currentOperator, firstOperand, secondOperand)
+    // normal operation
+    } else {
+        secondOperand = currentOperandTextEl.innerText
+        resetDisplays()
+        operate(currentOperator, firstOperand, secondOperand)
+    }
+    
 }
 
 
@@ -84,34 +89,32 @@ allClearButton.addEventListener('click', () => {
     allClear()
 })
 
-equalsButton.addEventListener('click', () => {
-    equals()
-})
+equalsButton.addEventListener('click', () => equals())
 
 //console.log button for testing data!!!
 deleteButton.addEventListener('click', () => {
     console.log(`${currentOperator} this is currentOperator`)
     console.log(`${firstOperand} this is firstOperand`)
     console.log(`${secondOperand} this is secondOperand`)
-    deleteNumber()
+    // deleteNumber()
 })
 //console.log button for testing data!!!
 
 //OPERATORS
 let add = (a, b) => {
-    firstOperand = a + b
+    currentOperandTextEl.innerText = a + b
 } 
 
 let subtract = (a, b) => {
-    firstOperand = a - b 
+    currentOperandTextEl.innerText = a - b 
 } 
 
 let multiply = (a, b) => {
-    firstOperand = a * b
+    currentOperandTextEl.innerText = a * b
 } 
 
 let divide = (a, b) => {
-    firstOperand = a / b
+    currentOperandTextEl.innerText = a / b
 } 
 
 let operate = (operator, a, b) => {
